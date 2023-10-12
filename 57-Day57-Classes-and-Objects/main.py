91a97e56d3812a1d7b1d351e34a7267d239275ac
Rishi Kashyap <121276276+rishikashyap99@users.noreply.github.com>
2023-10-12 13:32:00 +0530

    some chanes

--- 57-Day57-Classes-and-Objects/main.py
+++ 57-Day57-Classes-and-Objects/main.py
@@ -20,3 +20,29 @@ b.occupation = "HR"
 a.info()
 b.info()
 c.info()
+
+class employee():
+  name="shiavni"
+  company= "google"
+  salary=55000
+  doj="11/7/2022"
+  address="uttarkhand"
+  def job_des(self):
+    print("my name is", self.name , "working in the company" , self.company , "as a programmer")
+
+first_emp=employee()
+second_emp=employee()
+third_emp=employee()
+first_emp.name="shiv"
+second_emp.name="neha"
+first_emp.company="colab"
+second_emp.company="slides"
+third_emp.company="inspector"
+
+
+
+print(first_emp.company)
+print(second_emp.company)
+print(second_emp.doj)
+first_emp.job_des()
+third_emp.job_des()
